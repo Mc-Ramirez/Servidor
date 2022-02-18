b460098decae8704010df9d520a183b5ab658492
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-02-18 20:56:38 +0100

    no cargan los planetas en la consola de la web

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const { use } = require('./routes/planet');
 
 //Creamos el servidor
 const app = express();
 
 //Conectamos a la base de datos
-conectarDB(); 
+conectarDB();
+//app.use(cors());
 
 app.use(express.json());
 
@@ -14,3 +16,4 @@ app.use('/api/planets', require('./routes/planet'));
 app.listen(4000, () =>{
     console.log('El servidor esta corriendo perfectamente')
 })
+ 
