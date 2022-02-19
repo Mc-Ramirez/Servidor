1397208ecc89716b0da8744e933291a754079fae
SergioRivera01 <sergiorivera01@gmail.com>
2022-02-19 13:56:54 +0100

    Conectado con el cliente de manera satisfactoria

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const conectarDB = require('./config/db');
-const { use } = require('./routes/planet');
+const cors = require("cors");
+const {
+    use
+} = require('./routes/planet');
 
 //Creamos el servidor
 const app = express();
 
 //Conectamos a la base de datos
 conectarDB();
-//app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
 app.use('/api/planets', require('./routes/planet'));
 
-app.listen(4000, () =>{
+app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente')
 })
- 
